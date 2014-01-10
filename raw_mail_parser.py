002f97b364f2da5b4e157cbdb3180298bfe696cc
Javran Cheng <Javran.c@gmail.com>
2014-01-09 22:22:10 -0500

    deal with html and plain text

--- raw_mail_parser.py
+++ raw_mail_parser.py
@@ -1,13 +1,45 @@
 #!/usr/bin/env python2
 
 import email
+import mailbox
 
+from HTMLParser import HTMLParser
+
+class EmailHTMLParser(HTMLParser):
+    def __init__(self):
+        HTMLParser.__init__(self)
+        self.parsed_content = ""
+
+    def handle_data(self,data):
+        self.parsed_content += data
+
+# load and parse file
 test_file = "./data/phishing1.mbox"
+mbox = mailbox.mbox(test_file)
 
-with open(test_file, "r") as f:
-    msg = email.message_from_file(f)
+def handle_text_plain(data):
+    print "Encounter plain text: %s" % data[:10]
 
-for k,v in msg.items():
-    print "%s ====> %s" % (k,v)
+def handle_text_html(data):
+    parser = EmailHTMLParser()
+    parser.feed(data)
+    print parser.parsed_content
 
-print msg.get_payload()[:1000]
+type_handlers = \
+    { "text/plain": handle_text_plain
+    , "text/html" : handle_text_html
+    }
+
+def handle_message(msg):
+    if msg.is_multipart():
+        # TODO: what are these messages?
+        print "<multimple messages>"
+    else:
+        print "# New email"
+        type_handlers[msg.get_content_type()](msg.get_payload())
+
+interesting_msgs = \
+    filter(lambda x: not x.is_multipart(), mbox)
+
+# handle each messages
+map(handle_message, interesting_msgs)
