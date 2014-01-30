1839e57d23617a9d818027af0917c7b01f9ee818
Javran Cheng <Javran.c@gmail.com>
2014-01-30 00:09:31 -0500

    full support for multipart msg

--- raw_mail_parser.py
+++ raw_mail_parser.py
@@ -17,31 +17,38 @@ class EmailHTMLParser(HTMLParser):
 test_file = "./data/phishing1.mbox"
 mbox = mailbox.mbox(test_file)
 
-def handle_text_plain(data):
-    print("Encounter plain text: %s" % data[:10])
+def handle_text_plain(msg):
+    data = msg.get_payload()
+    print("plain text: %s" % data[:40])
 
-def handle_text_html(data):
+def handle_text_html(msg):
+    data = msg.get_payload()
     parser = EmailHTMLParser()
     parser.feed(data)
-    print(parser.parsed_content)
+    print("html text: %s" % parser.parsed_content[:40])
 
 type_handlers = \
     { "text/plain": handle_text_plain
     , "text/html" : handle_text_html
     }
+    
+def default_handler(msg):
+    print("Unknown message of type: %s" % msg.get_content_type())
 
-def handle_message(msg):
+def handle_message(msg,level):
     if msg.is_multipart():
-        # TODO: what are these messages?
-        print("<multimple messages>")
+        print( ">>>>" * level )
+        for x in msg.get_payload():
+            handle_message(x,level+1)
+        print( "<<<<" * level )
     else:
-        print("--------- New email")
-        type_handlers[msg.get_content_type()](msg.get_payload())
-
-interesting_msgs = \
-    [x for x in mbox if not x.is_multipart()]
+        print("[Email begin]")
+        handler = type_handlers.get(
+                    msg.get_content_type(),
+                    default_handler)
+        handler(msg)
+        print("[Email end]")
 
 # handle each messages
-for m in interesting_msgs:
-    print("foo")
-    handle_message(m)
+for m in mbox:
+    handle_message(m,0)
